ec14afdcd8696b122d8e7e87837f80663cc1fc0b
微光北下 <wgbx@foxmail.com>
2025-01-23 18:14:15 +0800

    add code

--- apps/web/middleware.js
+++ apps/web/middleware.js
@@ -2,6 +2,13 @@ import { NextResponse } from "next/server";
 import cacheHandler from "./cache-handler";
 import {getData} from './utils'
 
+export default {
+  async fetch(request) {
+    // 处理请求逻辑
+    return new Response('Hello, world!', { status: 200 });
+  }
+};
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
   const { nextUrl } = request;
